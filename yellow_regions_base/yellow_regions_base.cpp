6bf24e573eca5db9ada4b2b7523d6377b54f1f38
Анисимова Мария <mar-anisimovaa@yandex.ru>
2021-08-27 10:38:22 +0300

    Init

---
+++ yellow_regions_base/yellow_regions_base.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <ctime>
+#include <tuple>
+using namespace std;
+
+/*enum class Lang {
+    DE, FR, IT
+};
+
+struct Region {
+    string std_name;
+    string parent_std_name;
+    map<Lang, string> names;
+    int64_t population;
+};*/
+
+bool operator<(const Region& lhs, const Region& rhs) {
+    return tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
+        tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
+}
+
+int FindMaxRepetitionCount(const vector<Region>& regions) {
+    map<Region, int> regions_to_number;
+    for (const Region& region : regions) {
+        regions_to_number[region]++;
+    }
+    int result = 0;
+    for (auto region : regions_to_number) {
+        result = (result > region.second) ? result : region.second;
+    }
+    return result;
+};
+
+/*int main() {
+    unsigned int start_time = clock(); 
+    cout << FindMaxRepetitionCount({
+        {
+            "Moscow",
+            "Russia",
+            {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+            89
+        }, {
+            "Russia",
+            "Eurasia",
+            {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+            89
+        }, {
+            "Moscow",
+            "Russia",
+            {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+            89
+        }, {
+            "Moscow",
+            "Russia",
+            {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+            89
+        }, {
+            "Russia",
+            "Eurasia",
+            {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+            89
+        },
+        }) << endl;
+
+    cout << FindMaxRepetitionCount({
+        {
+            "Moscow",
+            "Russia",
+            {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+            89
+        }, {
+            "Russia",
+            "Eurasia",
+            {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+            89
+        }, {
+            "Moscow",
+            "Russia",
+            {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou deux"}, {Lang::IT, "Mosca"}},
+            89
+        }, {
+            "Moscow",
+            "Toulouse",
+            {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+            89
+        }, {
+            "Moscow",
+            "Russia",
+            {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+            31
+        },
+        }) << endl;
+    unsigned int end_time = clock() - start_time;
+    cout << end_time << endl;
+
+    return 0;
+}*/
+
